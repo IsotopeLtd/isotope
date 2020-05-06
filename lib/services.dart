7fcce17324f33c3a0b83dd5c5da115dddc52386f
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2020-05-06 23:38:19 +1000

    Add analytics service.

--- lib/services.dart
+++ lib/services.dart
@@ -1,5 +1,6 @@
 library services;
 
+export 'src/services/analytics_service.dart';
 export 'src/services/cloud_storage_service.dart';
 export 'src/services/dialog_service.dart';
 export 'src/services/image_selection_service.dart';
