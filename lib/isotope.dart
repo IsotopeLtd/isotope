f43f5882d6197fedb03dd9d36d5231a571e298df
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2020-05-06 21:41:44 +1000

    Update pubspec and main library.

--- lib/isotope.dart
+++ lib/isotope.dart
@@ -1,7 +1,11 @@
 library isotope;
 
+export 'managers.dart';
+export 'models.dart';
 export 'platform.dart';
 export 'reactive.dart';
 export 'registrar.dart';
+export 'services.dart';
 export 'view_models.dart';
 export 'views.dart';
+export 'widgets.dart';
