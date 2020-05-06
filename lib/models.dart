b02b7425aca84d48cf8467fe231ef600432fb433
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2020-05-06 23:25:11 +1000

    Expose user model.

--- lib/models.dart
+++ lib/models.dart
@@ -4,3 +4,4 @@ export 'src/models/dialog_request.dart';
 export 'src/models/dialog_response.dart';
 export 'src/models/geographic_coordinate.dart';
 export 'src/models/navbar_item_model.dart';
+export 'src/models/user.dart';
