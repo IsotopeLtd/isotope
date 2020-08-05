c53f546abf5616149b5104093409c019076ccbbb
jocubeit <jurgenjocubeit@gmail.com>
2020-08-05 17:26:35 +1000

    Remove firebase services - now in isotope_firebase

--- lib/services.dart
+++ lib/services.dart
@@ -3,8 +3,6 @@ library services;
 export 'src/models/dialog_request.dart';
 export 'src/models/dialog_response.dart';
 
-export 'src/services/analytics_service.dart';
-export 'src/services/cloud_storage_service.dart';
 export 'src/services/dialog_service.dart';
 export 'src/services/media_service.dart';
 export 'src/services/location_service.dart';
